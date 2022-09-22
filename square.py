a5c73115cb8e643699249aff20fac2e7d4c16d9c
BlipBloopBeep <113717678+BlipBloopBeep@users.noreply.github.com>
2022-09-22 11:44:56 -0700

    Add files via upload

---
+++ square.py
@@ -0,0 +1,319 @@
+import turtle
+
+turtle.color("green", "lime")
+
+turtle.pencolor("green")
+turtle.speed(99)
+turtle.shape('turtle')
+
+t = 150
+c = 1
+while c == 1:
+
+    turtle.penup()
+    turtle.goto(-200, t)
+    turtle.pendown()
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    t = t-50
+    if t == -250:
+        break
+
+
+turtle.penup()
+turtle.goto(-200, 150)
+turtle.pendown()
+r = 0
+e = 1
+while e == 1:
+
+    turtle.forward(50)
+
+    y = 1
+    x = 0
+    while y == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, 100)
+turtle.pendown()
+r = 0
+d = 1
+while d == 1:
+
+
+    g = 1
+    x = 0
+    while g == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    turtle.forward(50)
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, 50)
+turtle.pendown()
+r = 0
+e = 1
+while e == 1:
+
+    turtle.forward(50)
+
+    y = 1
+    x = 0
+    while y == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, 0)
+turtle.pendown()
+r = 0
+d = 1
+while d == 1:
+
+
+    g = 1
+    x = 0
+    while g == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    turtle.forward(50)
+    r = r+1
+    if r == 4:
+        break
+
+
+turtle.penup()
+turtle.goto(-200, -50)
+turtle.pendown()
+r = 0
+e = 1
+while e == 1:
+
+    turtle.forward(50)
+
+    y = 1
+    x = 0
+    while y == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, -100)
+turtle.pendown()
+r = 0
+d = 1
+while d == 1:
+
+
+    g = 1
+    x = 0
+    while g == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    turtle.forward(50)
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, -150)
+turtle.pendown()
+r = 0
+e = 1
+while e == 1:
+
+    turtle.forward(50)
+
+    y = 1
+    x = 0
+    while y == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    r = r+1
+    if r == 4:
+        break
+
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+r = 0
+d = 1
+while d == 1:
+
+
+    g = 1
+    x = 0
+    while g == 1:
+        turtle.forward(1)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        x = x+1
+        if x == 25:
+            break
+    turtle.forward(50)
+    r = r+1
+    if r == 4:
+        break
+
+
+
